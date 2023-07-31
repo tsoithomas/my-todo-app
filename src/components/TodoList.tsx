5a5047bd57e583fefff30be4dd90f45e94ea484f
Thomas Tsoi <tsoithomas@outlook.com>
2023-07-31 16:01:14 -0400

    Completed onClick function to highlight selected item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 const TodoList = () => {
     const [selected, setSelected] = useState(0);
-
     const todos = ["Buy groceries", "Clean the house", "Walk the dog"]
+    const handleClick = (e: any, index: number) => {
+        setSelected(index);
+        document.querySelectorAll(".active").forEach((p) => {p.classList.remove("active")})
+        document.querySelector(".list-group-item:nth-child(" + (index+1) + ")")?.classList.add("active");
+    };
 
-    const formatted = todos.map((item) => {
-        return <li key={item}>{item}</li>
-    });
 
-    
+    const formatted = todos.map((item, index) => {
+                return <li className="list-group-item" onClick={(e) => {handleClick(e, index)}} data-index={index} key={item}>{item}</li>
+            });
 
     return (
-        <div>
-            <ol className="todolist">
+        todos.length > 0 
+        ?   <ul className="list-group">
                 {formatted}
-            </ol>
-        </div>
+            </ul>
+        : <>There are no items.</>
     )
 }
 export default TodoList
